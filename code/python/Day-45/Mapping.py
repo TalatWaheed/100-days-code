f1f42e79eb0f1322d19742bfa1f5e5d117824731
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-08-14 12:00:46 -0500

    day-45

---
+++ code/python/Day-45/Mapping.py
@@ -0,0 +1,8 @@
+numbers1 = dict({'x': 4, 'y': 5})
+print('numbers1 =',numbers1)
+
+numbers2 = {'x': 4, 'y': 5}
+print('numbers2 =',numbers2)
+
+numbers3 = dict({'x': 4, 'y': 5}, z=8)
+print('numbers3 =',numbers3)
