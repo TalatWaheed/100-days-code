0665d70dd3e6900e995cd819403ce62beea1c5b4
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-08-22 10:15:53 -0500

    day-53

---
+++ code/python/Day-53/List.py
@@ -0,0 +1,10 @@
+print(list())
+
+vowelString = 'aeiou'
+print(list(vowelString))
+
+vowelTuple = ('a', 'e', 'i', 'o', 'u')
+print(list(vowelTuple))
+
+vowelList = ['a', 'e', 'i', 'o', 'u']
+print(list(vowelList))
