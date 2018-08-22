a6d39ce6cedeb0f51ab769f2530ca83821d2f576
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-08-22 10:42:26 -0500

    day-53

---
+++ code/python/Day-53/Iter.py
@@ -0,0 +1,13 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+vowelsIter = iter(vowels)
+
+print(next(vowelsIter))
+
+print(next(vowelsIter))
+
+print(next(vowelsIter))
+
+print(next(vowelsIter))
+
+print(next(vowelsIter))
