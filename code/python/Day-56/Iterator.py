134f96df24daa543658430be88c30d722820d0b1
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-08-25 00:55:11 -0500

    day-56

---
+++ code/python/Day-56/Iterator.py
@@ -0,0 +1,11 @@
+random = [5, 9]
+
+randomIterator = iter(random)
+
+print(next(randomIterator, '-1'))
+
+print(next(randomIterator, '-1'))
+
+print(next(randomIterator, '-1'))
+print(next(randomIterator, '-1'))
+print(next(randomIterator, '-1'))
