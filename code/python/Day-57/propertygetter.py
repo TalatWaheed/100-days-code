2aec6a5067938ae1818474e2db44714fcea89a0d
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-08-26 03:38:11 -0500

    day-57

---
+++ code/python/Day-57/propertygetter.py
@@ -0,0 +1,25 @@
+class Person:
+    def __init__(self, name):
+        self._name = name
+
+    def getName(self):
+        print('Getting name')
+        return self._name
+
+    def setName(self, value):
+        print('Setting name to ' + value)
+        self._name = value
+
+    def delName(self):
+        print('Deleting name')
+        del self._name
+
+    name = property(getName, setName, delName, 'Name property')
+
+
+p = Person('Adam')
+print(p.name)
+
+p.name = 'John'
+
+del p.name
