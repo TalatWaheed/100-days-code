ee96119979df1ee7cc8e1650429dfa21ffd5eb2e
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-08-27 10:00:46 -0500

    day-58

---
+++ code/python/Day-58/Reversed.py
@@ -0,0 +1,10 @@
+seqString = 'Python'
+print(list(reversed(seqString)))
+
+seqTuple = ('P', 'y', 't', 'h', 'o', 'n')
+print(list(reversed(seqTuple)))
+seqRange = range(5, 9)
+print(list(reversed(seqRange)))
+
+seqList = [1, 2, 4, 3, 5]
+print(list(reversed(seqList)))
