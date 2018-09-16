61280179742bb44cac726c4b30a9a3e649fbc534
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-09-16 02:05:23 -0500

    day-78

---
+++ code/python/Day-78/NumArray.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.arange(12)
+#print(a)
+#print(a[2:5])
+a2 = np.array([[1,2,3],[3,4,5],[4,5,6],[6,7,8]])
+#print(a2)
+print(a2[1:])  #print index 1 to all means [3,4,5],[4,5,6],[6.7.8]
+print(a2[2:])   #print index 2 to all 
+print(a2[:3])   #print starting to index 2 means [1,2,3],[3,4,5],[4,5,6]
