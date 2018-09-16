bb93c34b5aab003cde22b81a34ad6d64c3b8c9ed
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-09-16 02:03:09 -0500

    day-78

---
+++ code/python/Day-78/mix.py
@@ -0,0 +1,17 @@
+phrase = "Talat Waheed"
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.isupper())
+
+phrase = "Simran Singh"
+print(phrase[0])
+
+print(3 * (4 + 5))
+print(10 % 3)
+
+my_num = 5
+print(my_num)
+
+print(str(my_num))
+
+print(max(4,6))
