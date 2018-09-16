02c616200cd58552d63abb09a80cc9ba817ca7ea
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-09-16 02:03:42 -0500

    day-78

---
+++ code/python/Day-78/balance.py
@@ -0,0 +1,10 @@
+def matched(str):
+    count = 0
+    for i in str:
+        if i == "(":
+            count += 1
+        elif i == ")":
+            count -= 1
+        if count < 0:
+            return False
+    return count == 0
