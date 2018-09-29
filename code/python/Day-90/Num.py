ac600353de524eaf80eb5f4ccfc7db75a58ea2aa
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-09-29 10:32:20 -0500

    day-90

--- code/python/Day-90/Num.py
+++ code/python/Day-90/Num.py
@@ -1,5 +1,12 @@
-def num:
- a=1
- b=2
- c=a+b
- print(c)
+fname = input("Enter file name: ")
+k = 0
+ 
+with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            for letter in i:
+                if(letter.isspace):
+                    k=k+1
+print("Occurrences of blank spaces:")
+print(k)
