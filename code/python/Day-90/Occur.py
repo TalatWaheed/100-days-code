ecaa527ea49e2acf753f190f0ecdd2314f5f2f81
TalatWaheed <35964435+TalatWaheed@users.noreply.github.com>
2018-09-29 10:33:34 -0500

    day-90

--- code/python/Day-90/Occur.py
+++ code/python/Day-90/Occur.py
@@ -1,1 +1,12 @@
-occurrence of given number.. using python 
+fname = input("Enter file name: ")
+word=input("Enter word to be searched:")
+k = 0
+ 
+with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            if(i==word):
+                k=k+1
+print("Occurrences of the word:")
+print(k)
